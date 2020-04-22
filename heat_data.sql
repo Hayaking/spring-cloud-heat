a11532f147a66fa7081d47da823fa8b1d6050aa1
hayaking <1028779917@qq.com>
2020-04-22 13:41:36 +0800

    update heat_data.sql

--- heat_data.sql
+++ heat_data.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 18/04/2020 16:29:36
+ Date: 22/04/2020 13:40:37
 */
 
 SET NAMES utf8mb4;
@@ -52,7 +52,7 @@ CREATE TABLE `consumer_config`  (
   `deleted` int(0) NULL DEFAULT NULL,
   `version` int(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for grps
@@ -66,6 +66,21 @@ CREATE TABLE `grps`  (
   `create_date` datetime(3) NULL DEFAULT NULL,
   `deleted` int(0) NULL DEFAULT NULL,
   `version` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for log
+-- ----------------------------
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `userId` int(0) NULL DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_date` datetime(3) NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作类型',
+  `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `operation` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -92,15 +107,16 @@ CREATE TABLE `user`  (
   `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `role_id` int(0) NULL DEFAULT NULL,
-  `create_date` datetime(3) NULL DEFAULT NULL,
-  `deleted` int(0) NULL DEFAULT NULL,
-  `version` int(0) NULL DEFAULT NULL,
+  `create_date` datetime(3) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(3),
+  `deleted` int(0) NULL DEFAULT 0,
+  `version` int(0) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'haya', '1024', NULL, NULL, NULL, '2020-03-19 16:23:09.000', NULL, NULL);
+INSERT INTO `user` VALUES (1, 'haya', '1024', NULL, NULL, NULL, '2020-04-19 19:38:55.859', 0, 0);
+INSERT INTO `user` VALUES (2, 'rose', '1024', NULL, NULL, NULL, '2020-04-19 19:59:37.996', 1, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
